cf76066bd203ab5f75e3d2926a40ba9ca5ce698b
Maximo Di napoli <maximodinapolimail@gmail.com>
2025-02-18 19:21:54 -0300

    ejercicio1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.model.DynamicBinaryMerkleTree;
+import org.example.model.StaticBinaryMerkleTree;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        DynamicBinaryMerkleTree tree = new DynamicBinaryMerkleTree();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Merkle Root Inicial: " + tree.getMerkleRoot());
+
+        tree.addLeft("Transaction1");
+        tree.addRight("Transaction2");
+        tree.addLeft("Transaction3");
+        tree.addRight("Transaction4");
+
+        System.out.println("Merkle Root después de agregar transacciones: " + tree.getMerkleRoot());
+
     }
 }
---
+++ src/main/java/org/example/model/StaticBinaryMerkleTree.java
@@ -0,0 +1,68 @@
+package org.example.model;
+
+
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class StaticBinaryMerkleTree implements BinaryMerkleTree {
+    private String hash;
+    private StaticBinaryMerkleTree left;
+    private StaticBinaryMerkleTree right;
+
+    public StaticBinaryMerkleTree(String data) {
+        this.hash = computeHash(data);
+        this.left = null;
+        this.right = null;
+    }
+
+    private String computeHash(String data) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(data.getBytes());
+            return Base64.getEncoder().encodeToString(encodedHash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error al calcular el hash", e);
+        }
+    }
+
+    @Override
+    public String getHash() {
+        return hash;
+    }
+
+    @Override
+    public StaticBinaryMerkleTree getLeft() {
+        return left;
+    }
+
+    @Override
+    public StaticBinaryMerkleTree getRight() {
+        return right;
+    }
+
+    @Override
+    public void addLeft(String data) {
+        this.left = new StaticBinaryMerkleTree(data);
+        updateHash();
+    }
+
+    @Override
+    public void addRight(String data) {
+        this.right = new StaticBinaryMerkleTree(data);
+        updateHash();
+    }
+
+    private void updateHash() {
+        String leftHash = (left != null) ? left.getHash() : "";
+        String rightHash = (right != null) ? right.getHash() : "";
+        this.hash = computeHash(leftHash + rightHash);
+    }
+
+    @Override
+    public String getMerkleRoot() {
+        return this.hash;
+    }
+
+}
