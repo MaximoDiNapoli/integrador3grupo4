c56f349209caf96e4d01c10b3ce6a936f4389e1b
MELI753753 <meliselles@gmail.com>
2025-02-18 19:16:20 -0300

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,18 @@
-package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ArbolBinarioBusqueda arbol1 = new ArbolBinarioBusqueda();
+        arbol1.insertar(5);
+        arbol1.insertar(3);
+        arbol1.insertar(7);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ArbolBinarioBusqueda arbol2 = new ArbolBinarioBusqueda();
+        arbol2.insertar(4);
+        arbol2.insertar(2);
+        arbol2.insertar(8);
+
+        ArbolBinarioBusqueda arbolCombinado = ArbolBinarioBusqueda.combinarArboles(arbol1, arbol2);
+
+        System.out.println("Árbol combinado (inorden): ");
+        arbolCombinado.inorden(arbolCombinado.raiz);
     }
 }
